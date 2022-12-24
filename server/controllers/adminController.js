7f24b2b7e321a862c183b936ad90d7ee39146778
shch989 <tankpop11@gmail.com>
2022-12-24 22:32:52 +0900

    상품명 검색시 내용 출력 코드

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -9,12 +9,34 @@ module.exports.postItem = (req, res, next) => {
   })
 }
 
+module.exports.postFindItem = (req, res, next) => {
+  const query = { title: req.body.title }
+  Item.find(query, (err, items) => {
+    if (err) {
+      return next(err)
+    }
+    if (items.length === 0) {
+      return res
+        .status(404)
+        .send({ message: '해당 내용의 상품이 존재하지 않습니다.' })
+    }
+    const findItems = items.map((item) => ({
+      title: item.title,
+      price: item.price,
+    }))
+    res.send(findItems)
+  })
+}
+
 module.exports.getItems = (req, res, next) => {
   Item.find({}, (err, items) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
-    const allItems = items.map(item => ({ title: item.title, price: item.price }));
-    res.send(allItems);
+    const allItems = items.map((item) => ({
+      title: item.title,
+      price: item.price,
+    }))
+    res.send(allItems)
   })
 }
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const { postItem, getItems } = require('../controllers/adminController')
+const { postItem, getItems, postFindItem } = require('../controllers/adminController')
 
 // /admin/product => POST
 router.post('/product', postItem)
 router.get('/product', getItems)
+router.post('/product-find', postFindItem)
 
 module.exports = router;
