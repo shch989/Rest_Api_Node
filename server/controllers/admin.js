44ed2d60866486e2049d892cddb430269ce76dca
shch989 <tankpop11@gmail.com>
2022-12-25 18:15:55 +0900

    상품 정보 수정기능 추가

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -4,13 +4,32 @@ exports.postAddProduct = (req, res, next) => {
   const item = new Item(req.body)
   item.save((err) => {
     if (err) return res.json({ messgae: err })
-    return res
-      .status(200)
-      .json({
+    return res.status(200).json({
+      title: item.title,
+      imageUrl: item.imageUrl,
+      description: item.description,
+      price: item.price,
+    })
+  })
+}
+
+exports.putEditProduct = (req, res, next) => {
+  const itemId = req.params.itemId
+  const updatedItem = req.body
+  Item.findById(itemId, (err, item) => {
+    if (err) return res.json({ messgae: err })
+    item.title = updatedItem.title
+    item.imageUrl = updatedItem.imageUrl
+    item.description = updatedItem.description
+    item.price = updatedItem.price
+    item.save((err) => {
+      if (err) return res.json({ messgae: err })
+      return res.status(200).json({
         title: item.title,
         imageUrl: item.imageUrl,
         description: item.description,
         price: item.price,
       })
+    })
   })
 }
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,5 +4,7 @@ const adminController = require('../controllers/admin')
 
 // /admin/add-product => POST <상품 등록>
 router.post('/add-product', adminController.postAddProduct)
+// /admin/:itemId/edit-product => PUT <상품 수정>
+router.put('/:itemId/edit-product', adminController.putEditProduct)
 
 module.exports = router;
